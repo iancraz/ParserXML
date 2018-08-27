70ff06047b170f4e01b57b7e6e5eb3bf39a181ae
idiaz97 <idiaz@itba.edu.ar>
2018-08-26 22:59:24 -0300

    Added Apache LICENSE

--- Source/parseXML.cpp
+++ Source/parseXML.cpp
@@ -1,5 +1,21 @@
+/*
+Copyright 2018 Ian Cruz Diaz
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #include "parseXML.h"
 #define SIZE	100000
+
 using namespace std;
 
 void startTagHandler(void *userData, const XML_Char *name, const XML_Char **atts);
@@ -83,7 +99,7 @@ void startTagHandler(void *userData,const XML_Char *name,const XML_Char **atts)
 		}
 		else if (strcmp(name, "title") == 0)
 			data->state = CHANNEL_TITLE;
-		else 
+		else
 			data->state = NOT_RELEVANT_CH;
 		break;
 	case NEWS:
